382f99a5df421763ddfac4144e70dd578b2c7fab
student <dhirajchavan355@gmail.com>
2024-01-21 22:29:47 +0530

    created new project

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import{BrowserRouter,Routers,Route} from 'react-router-dom';
+import {Box} from '@mui/material';
+
+const App = () => {
+  return (
+    <div>App</div>
+  )
+}
+
+export default App
